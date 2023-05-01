28706577e6664984e6ddd5687497a53d86113c6c
bouldja <lounisboulja@gmail.com>
2023-05-01 22:08:13 +0200

    update footer

--- apps/admin/src/app/pages/users/users.component.ts
+++ apps/admin/src/app/pages/users/users.component.ts
@@ -2,25 +2,35 @@ import {Component, ViewEncapsulation} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ButtonToggleComponent} from "@d2c/shared/button-toggle/button-toggle.component";
 import {ToggleButton} from "@d2c/shared/button-toggle/toggle-button";
-import {MatExpansionModule} from "@angular/material/expansion";
+import {MatExpansionModule, MatExpansionPanel} from "@angular/material/expansion";
+import {CdkAccordionModule} from "@angular/cdk/accordion";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'd2c-users',
   standalone: true,
-  imports: [CommonModule, ButtonToggleComponent, MatExpansionModule],
+  imports: [CommonModule, ButtonToggleComponent, MatExpansionModule, CdkAccordionModule, MatIconModule],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class UsersComponent {
   selected: string | undefined;
-
-  items: ToggleButton[] = [
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  expandedIndex = 0;
+  data: ToggleButton[] = [
     {
       displayedValue: 'Yasmine ACHIR', value: 'yasmine', icon: 'woman'
     },
     {
       displayedValue: 'Lounis BOULDJA', value: 'lounis', icon: 'man'
-    }
+    },
   ]
+
+  navigate(item: string) {
+    console.log(item)
+  }
+
+  preventExpansion(event: Event, panel: MatExpansionPanel) {
+    panel.close();
+  }
 }
